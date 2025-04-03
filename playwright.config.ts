8b180d13aa51927ff1b245cf66e49624a8e38e9b
Matt <mbarrett@planittesting.com>
2025-04-03 23:44:50 +1300

    Initial commit after husky Removal

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,8 +23,9 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-      ['html', { outputFolder: 'playwright-report', open: 'never' }],
-      ['junit', { outputFile: 'playwright-report/results.xml' }]
+    ['list'], 
+    ['junit', { outputFile: 'playwright-report/results.xml' }],
+    ['html', { outputFile: 'playwright-report/results.html' }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
