cc5f91a2b7f92fed214b0bcdf2e38243721412f4
Matt <mbarrett@planittesting.com>
2025-04-05 00:59:46 +1300

    run eslint in pipeline -try new config file type

---
+++ eslint.config.mjs
@@ -0,0 +1,53 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+// eslint.config.mjs
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+export default define [
+  js.configs.recommended, // Base ESLint config
+
+  compat.config({
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error'],
+      "indent": ["error", 2],
+      "quotes": ["error", "single"],
+      "semi": ["error", "always"],
+      "no-unused-vars": ["error"],
+      "no-console": ["warn"],
+      "prefer-const": ["error"],
+      "no-unreachable": ["error"],
+      "no-empty-function": ["error"],
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+      "react/prop-types": ["error"],
+      "react/no-array-index-key": ["warn"],
+    },
+  }),
+
+  {
+    files: ['*.ts'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+  },
+];
